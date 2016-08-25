9de2cf4c1041db6856f2fe3bf04cf7351a83da43
Shoshan-z <shoshan.z@gmail.com>
2016-08-25 22:53:57 +0300

    *some updates to MainTmp

--- SoftwareProject/MainTmp.cpp
+++ SoftwareProject/MainTmp.cpp
@@ -1,22 +1,43 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 extern "C" {
 #include "SPConfig.h"
 }
 
 
 int main(int argc, char** argv) {
-	char* configFName = "spcbir.config";
+	char configFName[1024] = {0};
 	SPConfig config = NULL;
 	SP_CONFIG_MSG msg;
 
-	if (argc == 0) {
+	if (argc == 1) {
+		strcpy(configFName, "spcbir.config");
 		config = spConfigCreate(configFName, &msg);
+		if (msg == SP_CONFIG_CANNOT_OPEN_FILE) {
+			printf("The default configuration file spcbir.config couldn’t be open\n");
+			return 0;
+		}
 	}
 
-	if (msg == SP_CONFIG_CANNOT_OPEN_FILE) {
-		printf("“The default configuration file spcbir.config couldn’t be open\n");
+	if (argc == 2 || argc>3) {
+		printf("Invalid command line : use -c <config_filename>\n");
+		return 0;
 	}
+
+	if (strcmp(argv[1], "-c") != 0) {
+		printf("Invalid command line : use -c <config_filename>\n");
+		return 0;
+	}
+
+	config = spConfigCreate(configFName, &msg);
+	if (msg == SP_CONFIG_CANNOT_OPEN_FILE) {
+		printf("The configuration file %s couldn’t be open",configFName);
+		return 0;
+	}
+
+
+
 
 
 	return 0;
